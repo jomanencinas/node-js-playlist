861c09b9e4cc58548faa36326f9963fdd146c61c
Joman Encinas <jomanencinas@gmail.com>
2017-01-23 16:18:01 +0800

    Added node in .gitignore
    Created app.js, package.json

---
+++ app.js
@@ -0,0 +1,16 @@
+//Global Objects
+
+// setTimeout(function(){
+//   console.log('3 seconds have passed');
+// }, 3000)
+
+// var time = 0;
+// setInterval(function(){
+//   time+=2;
+//   console.log(time + ' seconds have passed');
+// }, 2000)
+
+console.log(__dirname);
+console.log(__filename);
+console.log(__dirname);
+console.log('hello');
